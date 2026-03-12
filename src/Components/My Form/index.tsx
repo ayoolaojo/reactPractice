e9587a025e9f57a5864a638e8ea02714664650d9
Ayoola <johnsonayoolaojo@gmail.com>
2026-03-12 08:58:28 +0100

    form handling practice

--- src/Components/My Form/index.tsx
+++ src/Components/My Form/index.tsx
@@ -1,79 +1,46 @@
-import { useState } from "react";
+import { useState } from "react"
+
 
 const MyForm = () => {
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    age : '',
-  });
+       const [inputData,setInputData] = useState({
+          fullName : '',
+          email:'',
+          password: '',
+          phone: '',
+          age : '',
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
 
-    const {name, value} = e.target
-    setFormData({
-        ...formData, [name]:value
-    })
+       })
 
-  }
- 
+       const handleInput = (e:React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+          const {name, value} = e.target
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
-     e.preventDefault()
+          setInputData((prev)=>({
+            ...prev ,
+             [name] : value
+          }))
+       }
 
-     console.log (formData)
+       const handleSubmit = (e:React.FormEvent)=> {
+        e.preventDefault()
+        console.log(inputData)
 
-  }
+       }
+  
   return (
-    <div className=" h-screen w-screen flex flex-col justify-center align-center ">
-      <h1>My Form</h1>
-      <p>
-        Ensure to fill the form below accordingly to have access to grant of $5m
-      </p>
-
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="First Name"
-          name="firstName"
-          onChange={handleChange}
-        />
-        
-        <br /> <br />
-
-        <input
-          type="text"
-          placeholder="Last Name"
-          name="lastName"
-          onChange={handleChange}
-        />  <br /> <br />
-
-        <input
-         type="text"
-         placeholder="Email"
-         name="email"
-         onChange={handleChange}
-          /> <br /> <br />
-
-          <input 
-          type="text" 
-          placeholder="password"
-          name="password"
-          onChange={handleChange}
-          />
-
-        <br /> <br />
-        <input
-          type="number"
-          placeholder="Age :"
-          onChange={handleChange}
-        />
-        <br /> <br />
-        <button type="submit">Apply Now</button>
-      </form>
+    <div className="flex flex-col jutify-center align-center ">
+           <h1>Registration Form</h1>
+           <form  action="" onSubmit={handleSubmit}>
+             <input type="text" placeholder="Full name" name="fullname" onChange={handleInput}/>  <br /> <br />
+             <input type="email" placeholder="Email address" name="email" onChange={handleInput}/> <br /> <br />
+             <input type="password" placeholder="Password" name="password" onChange={handleInput}/> <br /> <br />
+             <input type="tel" placeholder="Phone Number" name="phone" onChange={handleInput} /> <br /> <br />
+             <input type="number" placeholder="Age" name="age" onChange={handleInput} /> <br /> <br />
+             <button type="submit">Register</button> <br /> <br />
+           </form>
+      
     </div>
-  );
-};
+  )
+}
 
-export default MyForm;
+export default MyForm
