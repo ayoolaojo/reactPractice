4220f5bf8e4048836188bdce96bfad9dfed2262e
Ayoola <johnsonayoolaojo@gmail.com>
2026-03-25 14:05:53 +0100

    update

--- src/Components/MyNewForm/index.tsx
+++ src/Components/MyNewForm/index.tsx
@@ -1,26 +1,37 @@
-import { useState } from "react"
-
+import { useState } from "react";
 
 const MyNewForm = () => {
-    const [formData, setFormData] = useState({
-        fullName: '',
-        email:  '',
-        Phone: '',
-        location: '',
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    Phone: "",
+    location: "",
+  });
 
+  const handleInputChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+  ) => {
+    const { name,value } = e.target;
+    setFormData((prev)=>({
+      ...prev,
+      [name] : value
+    }))
 
+  };
 
-
-    })
+  const handleSubmit = (e:React.FormEvent) =>{
+     e.preventDefault()
+     console.log(formData)
+  }
   return (
     <form className="h-[50vh] w-full bg-blue-700 flex flex-col justify-center items-center">
-         <input type="text" placeholder="fullName" /> <br /> <br />
-         <input type="text" placeholder="email" />  <br /> <br />
-         <input type="text" placeholder="Phone" />  <br /> <br />
-         <input type="text" placeholder="Location" /> <br /> <br />
-         <button type="submit"></button>
+      <input onChange={handleInputChange} type="text" placeholder="fullName" name="fullName" /> <br /> <br />
+      <input onChange={handleInputChange} type="text" placeholder="email" name="email" /> <br /> <br />
+      <input onChange={handleInputChange} type="text" placeholder="Phone" name="Phone" /> <br /> <br />
+      <input onChange={handleInputChange} type="text" placeholder="Location" name="location" /> <br /> <br />
+      <button type="submit" onSubmit={handleSubmit}></button>
     </form>
-  )
-}
+  );
+};
 
-export default MyNewForm
+export default MyNewForm;
